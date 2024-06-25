963a1866abb83a77f04341cf13983f0a8622dcd3
PC <PC@DESKTOP-0GSI119>
2024-06-25 22:12:57 +0700

    Add project files.

---
+++ Application/IGenericRepository/Imp/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IGenericRepository.Imp
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomClothingStore/Controllers/StockController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomClothingStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+    }
+}
